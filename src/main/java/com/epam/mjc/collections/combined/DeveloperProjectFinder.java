f8f4c91b2295478882a556f1bc8242bf5c746722
kukumaj <zakumaj@gmail.com>
2022-10-24 23:30:38 +0200

    done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,34 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()
+        ) {
+            if (entry.getValue().contains(developer)) {
+                list.add(entry.getKey());
+            }
+        }
+        Collections.reverse(list);
+        list.sort(new LengthNameComparator());
+
+        return list;
+    }
+}
+
+class LengthNameComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        int size1 = o1.length();
+        int size2 = o2.length();
+        if (size1 < size2) {
+            return 1;
+        } else if (size1 > size2) {
+            return -1;
+        }
+        return 0;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,25 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> map = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()
+        ) {
+            int length = entry.getKey().length();
+            Set<String> set = new HashSet<>();
+            for (Map.Entry<String, Integer> entry1 : sourceMap.entrySet()
+            ) {
+                if (length == entry1.getKey().length()) {
+                    set.add(entry1.getKey());
+                }
+            }
+            map.put(length, set);
+        }
+        return map;
     }
 }
